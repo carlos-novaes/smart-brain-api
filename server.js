101915237a10d741a9eceabc72ca24f4c7931c3b
Carlos Novaes <carlosnovaes@poli.ufrj.br>
2019-01-31 00:28:18 -0200

    adding port

--- server.js
+++ server.js
@@ -48,7 +48,9 @@ app.post('/imageurl', (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
 
 /*
 
