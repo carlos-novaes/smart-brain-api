56ac72d00ae78087795f85a876e377a065cbf20d
Carlos Novaes <carlosnovaes@poli.ufrj.br>
2019-01-30 22:40:49 -0200

    add validation to backend, fixed bug where we could register an user with blank username and password

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+  apiKey: '01d0b0b27a8f4856ad852967a347af69'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
@@ -11,5 +27,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-  handleImage
+  handleImage,
+  handleApiCall
 };
